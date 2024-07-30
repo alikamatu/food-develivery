eaf886fd42c841cbdb907456e22618921ae04d4e
alikamatu <122170675+alikamatu@users.noreply.github.com>
2024-07-30 16:38:04 +0000

    Update List.jsx

--- admin/src/pages/list/List.jsx
+++ admin/src/pages/list/List.jsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react'
 
 const List = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://food-develivery-backend.onrender.com"
   const [list, setList] = useState([]);
 
   const fetchList = async () => {
