241cef65e938340233ed8f3416a17183b7b35785
alikamatu <122170675+alikamatu@users.noreply.github.com>
2024-07-30 03:24:51 +0000

    Update Add.jsx

--- admin/src/pages/add/Add.jsx
+++ admin/src/pages/add/Add.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify'
 
 const Add = () => {
 
-    const url = 'http://localhost:1000';
+    const url = 'http://localhost:10000';
     const [image, setImage] = useState(false);
     const [data, setData] = useState({
         name: "",
